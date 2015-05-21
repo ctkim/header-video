6c49fba5b7f5a776ff0aa69d40be3897ec89a00f
Christine Kim <kkej0630@gmail.com>
2015-05-21 14:33:17 -0400

    only video teaser

--- js/script.js
+++ js/script.js
@@ -2,48 +2,20 @@ var HeaderVideo = (function ($, document) {
 
     var settings = {
         container: $('.header-video'),
-        video: '#video',
-        header: $('.header-video--media'),
-        videoTrigger: $('#video-trigger'),
-        videoCloseTrigger: $('#video-close-trigger'),
-        teaserVideo: $('#teaser-video'),
-        autoPlayVideo: false
+        teaserVideo: $('.teaser-video')
     };
 
     var init = function(options){
         settings = $.extend(settings, options);
         getVideoDetails();
         setFluidContainer();
-        bindClickActions();
-        settings.videoCloseTrigger.hide();
-        
-        if(videoDetails.teaser) {
-            appendTeaserVideo();
-        }
-
-        if(settings.autoPlayVideo) {
-            appendFrame();
-        }
-    };
-
-    var bindClickActions = function() {
-        settings.videoTrigger.on('click', function(e) {
-            e.preventDefault();
-            appendFrame();
-            settings.videoCloseTrigger.fadeIn();
-        });
-        settings.videoCloseTrigger.on('click', function(e){
-            e.preventDefault();
-            removeFrame();
-        });
+        appendTeaserVideo();
     };
 
     var getVideoDetails = function() {
         //Get all the data attributes from the HTML container and return them as an object for easy data retrieval
         videoDetails = {
-            id: settings.header.attr('data-video-src'),
             teaser: settings.header.attr('data-teaser-source'),
-            provider: settings.header.attr('data-provider').toLowerCase(),
             videoHeight: settings.header.attr('data-video-height'),
             videoWidth: settings.header.attr('data-video-width')
         };
@@ -77,50 +49,6 @@ var HeaderVideo = (function ($, document) {
                 html = '<video autoplay="true" loop="loop" muted id="teaser-video" class="teaser-video"><source src="'+source+'.mp4" type="video/mp4"><source src="'+source+'.ogv" type="video/ogg"></video>';
             settings.container.append(html);
         }
-    };
-    
-    var createFrame = function() {
-        //Added an ID attribute to be able to remove the video element when the user clicks on the remove button
-        if(videoDetails.provider === 'youtube') {
-            var html = '<iframe id="video" src="http://www.youtube.com/embed/'+videoDetails.id+'?rel=0&amp;hd=1&autohide=1&showinfo=0&autoplay=1&enablejsapi=1&origin=*" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-        }
-        else if(videoDetails.provider === 'vimeo') {
-            var html = '<iframe id="video" src="http://player.vimeo.com/video/'+videoDetails.id+'?title=0&amp;byline=0&amp;portrait=0&amp;color=3d96d2&autoplay=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-        }
-        else if(videoDetails.provider === 'html5') {
-            var html = '<video autoplay="true" loop="loop" id="video"><source src="'+videoDetails.id+'.mp4" type="video/mp4"><source src="'+videoDetails.id+'.ogv" type="video/ogg"></video>';
-        }
-        return html;
-    };
-
-    var appendFrame = function() {
-        settings.header.hide();
-        settings.container.append(createFrame());
-        removePlayButton();
-        settings.teaserVideo.hide();
-    };
-
-    var removeFrame = function() {
-        $(settings.video).remove();
-        settings.teaserVideo.fadeIn();
-        displayPlayButton();
-        removeRemoveButton();
-    };
-
-    var removePlayButton = function () {
-        if(settings.videoTrigger) {
-            settings.videoTrigger.fadeOut('slow');
-        }
-    };
-
-    var displayPlayButton = function() {
-        if(settings.videoTrigger) {
-            settings.videoTrigger.fadeIn('slow');
-        }
-    };
-
-    var removeRemoveButton = function() {
-        settings.videoCloseTrigger.hide();
     };
 
     var isMobile = function () {
